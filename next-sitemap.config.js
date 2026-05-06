44b0c1895682d24beaa7cd8312785f7f08aaa7f2
seabert <351338629@qq.com>
2026-05-06 18:05:44 +0800

    修改 sitemap.xml 生成

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@
 module.exports = {
     siteUrl: 'https://sleepchoiceguide.com', // 你的域名
     generateRobotsTxt: true, // 必须开启，省去手动维护 robots.txt
+    generateIndexSitemap: false,
     sitemapSize: 7000,
     changefreq: 'daily', // 告诉 Google 我们每天都在监控价格
     priority: 0.7,
@@ -18,9 +19,6 @@ module.exports = {
                 allow: '/',
                 disallow: ['/api/*', '/admin/*'], // 保护你的自动化接口
             },
-        ],
-        additionalSitemaps: [
-            'https://sleepchoiceguide.com/sitemap.xml', // 如果后续有动态 SSR 页面
-        ],
+        ]
     },
 }
